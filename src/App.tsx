55b947267b472b19ac15f7f2f18f711882d64efe
ranggakrisnaa <infinity.duos@gmail.com>
2025-03-20 07:05:48 +0700

    feat: completed test hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AppRoutes from './routes';
 const App = () => {
   return (
     <>
+      // add comment
       <div className="w-screen h-screen flex justify-center items-center flex-col space-y-8">
         <AppRoutes />
       </div>
