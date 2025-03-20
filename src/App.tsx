39b668956ec76776bd87a15643b237806e0566fb
Rangga <111489517+ranggakrisnaa@users.noreply.github.com>
2025-03-20 07:09:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import AppRoutes from './routes';
 const App = () => {
   return (
     <>
-      // add comment
       <div className="w-screen h-screen flex justify-center items-center flex-col space-y-8">
         <AppRoutes />
       </div>
